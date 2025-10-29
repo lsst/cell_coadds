d9070a4f7a99aebfb7bd86304519a247c32d4b54
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-10-29 14:46:03 -0400

    Bump FILE_FORMAT_VERSION

--- python/lsst/cell_coadds/_fits.py
+++ python/lsst/cell_coadds/_fits.py
@@ -114,7 +114,7 @@ from ._single_cell_coadd import CoaddInputs, SingleCellCoadd
 from ._uniform_grid import UniformGrid
 from .typing_helpers import SingleCellCoaddApCorrMap
 
-FILE_FORMAT_VERSION = "0.6"
+FILE_FORMAT_VERSION = "0.7"
 """Version number for the file format as persisted, presented as a string of
 the form M.m, where M is the major version, m is the minor version.
 """
